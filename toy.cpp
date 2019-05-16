83301b1c2208180e274b816ba9cb19a345213304
Keith <pixxf@me.com>
2019-05-16 15:44:44 +0800

    code generation

--- toy.cpp
+++ toy.cpp
@@ -105,12 +105,24 @@ static int gettok() {
 
 
 namespace {
+
+
   /// ExprAST - Base class for all expression nodes.
   class ExprAST {
   public:
     virtual ~ExprAST() = default;
+
+
+    // The codegen() method says to emit IR for that AST node
+    // along with all the things it depends on,
+    // all they all return an LLVM Value object.
+    // Value is the class used to represent a "Static Signle Assignment(SSA) register" or "SSA value"
+    // in LLVM.
+    // The most distinct aspect of SSA values is that their value is computed as the related instruction
+    // executs, and it does not get new value until (and if) the instrcution re-executes.
     virtual Value *codegen() = 0;
   };
+
 
   /// NumberExprAST - Expression class for numeric literals like "1.0".
   class NumberExprAST : public ExprAST {
@@ -461,20 +473,47 @@ static std::unique_ptr<PrototypeAST> ParseExtern() {
 //===--------------========//
 // Code Generation
 //===--------------========//
+
+// The static variables will be used during code generation.
+// TheContext is an opaque object that owns a lot of core LLVM data structures,
+// such as the type and constant value tables.
 static LLVMContext TheContext;
+
+// The builder object is a helper object that makes it easy to generate LLVM instructions.
+// Instances of the IRBuilder class template keep track of the current place to insert instructions
+// and has methods to create new instructions.
 static IRBuilder<> Builder(TheContext);
+
+// TheModule is an LLVM construct that contains functions and global variables.
+// In many ways, it is the top-level structure that the LLVM IR uses to contain code.
+// It will own the memory for all of the IR that we generate, which is why the codegen() method
+// returns a raw Value*, rather than a unique_ptr<Value>.
 static std::unique_ptr<Module> TheModule;
+
+// The NamedValues map keeps track of which values are defined in the current scope and what
+// their LLVM representation is. (In other words, it is a symbol table for the code).
+// In this form of Kaleidoscope, the only things that can be referenced are function parameters.
+// As such, function parameters will be in this map when generating code for their function body.
 static std::map<std::string, Value*> NamedValues;
+
 
 Value* LogErrorV(const char* Str){
   LogError(Str);
   return nullptr;
 }
 
+
+// In the LLVM IR, numeric constants are represented with the ConstatntFP class,
+// with holds the numeric value in an APFloat internally (APFloat has the capability of holding floating
+// point constants of Arbitrary Precision ).
+// This code basically just creates and returns a ConstantFP.
+// Note that in the LLVM IR that constants are all uniqued together and shared.
 Value* NumberExprAST::codegen() {
   return ConstantFP::get(TheContext, APFloat(Val));
 }
 
+
+// In practice, the only values that can be in the NamedValues map are function arguments.
 Value* VariableExprAST::codegen() {
   // Look this variable up in the fucntion
   Value* V = NamedValues[Name];
@@ -482,6 +521,19 @@ Value* VariableExprAST::codegen() {
   return V;
 }
 
+
+// The basic idea here is that we recursively emit code for the left-hand side
+// of the expression, then the right-hand side,
+// then we compute the result of the binary expression.
+// In this code, we do a simple switch on the opcode to create the right LLVM instruction.
+// IRBuilder knows where to insert the newly created instruction, all you have to do is
+// specify what instruction to create (e.g. with CreateFAdd), which operands to use (L and R here)
+// and optionally provide a name for the generated instruction.
+// LLVM instructions are constrained by strict rules: for example, the Left and Right operators
+// of an add instruction must have the same type, and the result type of the add must match the
+// operand types.
+// On the other hand, LLVM specifies that the fcmp instruction always return an 'i1' value
+// (a one bit integer).
 Value* BinaryExprAST::codegen() {
   Value* L = LHS->codegen();
   Value* R = RHS->codegen();
@@ -496,12 +548,22 @@ Value* BinaryExprAST::codegen() {
     return Builder.CreateFMul(L, R, "multmp");
   case '<':
     L = Builder.CreateFCmpULT(L, R, "cmptmp");
+    // Convert bool 0/1 to double 0.0 or 1.0
     return Builder.CreateUIToFP(L, Type::getDoubleTy(TheContext), "booltmp");
   default:
     return LogErrorV("invalid binary operator");
   }
 }
 
+
+// The code following initially does a function name lookup in the LLVM Module's symbol table.
+// Recall that the LLVM Module is the container that holds the fucntions we are JIT'ing.
+// By giving each function the same name as what the user specifies, we can use the LLVM symbol table
+// to resolve function names for us.
+// Once we have the function to call, we recursively codegen each argument that is to be passed in,
+// and create an LLVM call instruction.
+// Note that LLVM uses the native C calling convetions by default, allowing these calls to also
+// call into standard library functions like "sin" or "cos" with no additional effort.
 Value* CallExprAST::codegen() {
   // Loop up the name in the global module table.
   Function* CalleeF = TheModule->getFunction(Callee);
@@ -520,6 +582,34 @@ Value* CallExprAST::codegen() {
   return Builder.CreateCall(CalleeF, ArgsV, "calltmp");
 }
 
+
+// Note first that this function returns a "Function*" instead of a "Value*".
+// Because a "prototype" really talks about the external interface for a function
+// (not the value computed by an expression), it makes sense for it to return
+// the LLVM Function it corresponds to when codegen'd.
+// The call to FunctionType::get creates the FunctionType that should be used
+// for a given Prototype. Since all function arguments in Kaleidoscope are of
+// type double, the first line creates a vector of "N" LLVM double types. It then
+// uses the FunctionType::get method to create a function type that takes "N" doubles
+// as arguments, returns one double as a result, and that is not vararg (the false
+// parameter indicates this).
+// Note that Types in LLVM are uniqued just like Constants are, so you don't
+// "new" a type, you "get" it.
+// The final line above actually creates the IR Function corresponding to the Prototype.
+// This indicates the type, linkage and name to use, as well as which module to insert into.
+// "external linkage" means the function may be defined outside the current module and/or
+// that it is callable by functions outside the module.
+// The Name passed in is the name the user specified: since "TheModule" is specified, this
+// name is registered in "TheModule"'s symbol table.
+// Finally, we set the name of each of the function's arguments according to the names given
+// in the Prototype.
+// This step isn't strictly necessary, but keeping the names consistent makes the IR more
+// readable, and allows subsequent code to refer directly to the arguments for their names,
+// rather than having to look up in the Prototype AST.
+// At this point we have a function prototype with no body.
+// This is how LLVM IR represents function declarations.
+// For extern statements in Kaleidoscope, this is as far as we need to go.
+// For function definitions however, we need to codegen and attach a function body.
 Function* PrototypeAST::codegen() {
   // make the function type: double(double, double) etc.
   std::vector<Type*> Doubles(Args.size(), Type::getDoubleTy(TheContext));
@@ -537,6 +627,13 @@ Function* PrototypeAST::codegen() {
   return F;
 }
 
+
+// For function definitions, we start by searching TheModule's symbol table for an existing version
+// of this function, in case one has already been created using an 'extern' statement.
+// If Module::getFunction return null then no previous version exists, so we'll codegen
+// one from the prototype.
+// In either case, we want to assert that the function is empty (i.e. has no body yet) before we start.
+
 Function* FunctionAST::codegen() {
   // First, check for an existing function from a previous 'extern' deaclaration.
   Function* TheFunction = TheModule->getFunction(Proto->getName());
@@ -544,26 +641,46 @@ Function* FunctionAST::codegen() {
   if (!TheFunction) TheFunction = Proto->codegen();
   if (!TheFunction) return nullptr;
 
+  // Now we get to the point where the Builder is set up.
+  // The first line creates a new basic block (named "entry"), which is inserted into TheFunction.
+  // The second line then tells the builder that new instructions should be inserted into the
+  // end of the new basic block.
+  // Basic blocks in LLVM are an important part of functions that define the Control Flow Graph.
+  // Since we don't have any control flow, our functions will only contain one block at this point.
+
   //Create a new basic block to start insertion into.
   BasicBlock* BB = BasicBlock::Create(TheContext, "entry", TheFunction);
   Builder.SetInsertPoint(BB);
 
+
+  // Next we add the function arguments to the NamedValues map (after first clearing it out)
+  // so that they're accessible to VariableExprAST nodes.
   // Record the function arguments in the NamedValues map.
   NamedValues.clear();
   for (auto& Arg : TheFunction->args())
     NamedValues[Arg.getName()] = &Arg;
 
+
+  // Once the insertion point has been set up and the NamedValues map populated,
+  // we call the codegen() method for the root expression of the function.
+  // If no error happens, this emits code to compute the expression into the entry block
+  // and returns the value that was computed.
+  //Assuming no error, we then create an LLVM ret instruction, which completes the function.
+  // Once the function is built, we call verifyFunction, which is provided by LLVM.
+  // This function does a variety of consistency checks on the generated code,
+  // to determine if our compiler is doing everything right.
+  // Using this is import: it can catch a log of bugs.
+  // Once the function is finished and validated, we return it.
   if (Value* RetVal = Body->codegen()) {
     Builder.CreateRet(RetVal);
     verifyFunction(*TheFunction);
 
     return TheFunction;
   }
-
+  // Error reading body, remove function.
   TheFunction->eraseFromParent();
   return nullptr;
 }
-
 
 
 
